91697367da8b7c63e6ef7142698d5000401e8f3d
Ruowen Qin <chinqrw@gmail.com>
2023-01-23 17:52:22 -0600

    Plugin draft

--- lua/user/hop.lua
+++ lua/user/hop.lua
@@ -1,12 +1,26 @@
 local M = {}
 
 M.config = function()
-  local status_ok, hop = pcall(require, "hop")
-  if not status_ok then
-    return
-  end
-  hop.setup()
-  require("user.keybindings").set_hop_keymaps()
+	local status_ok, hop = pcall(require, "hop")
+	if not status_ok then
+		return
+	end
+	hop.setup()
+	local directions = require("hop.hint").HintDirection
+	vim.keymap.set("", "f", function()
+		hop.hint_char1()
+	end, { remap = true })
+	vim.keymap.set("", "F", function()
+		hop.hint_anywhere({ current_line_only = true, direction = directions.AFTER_CURSOR })
+	end, { remap = true })
+	vim.keymap.set("", "<Leader>j", function()
+		hop.hint_patterns()
+	end, { remap = true })
+	vim.keymap.set("", "<Leader>k", function()
+		hop.hint_words()
+	end, { remap = true })
+
+	hop.setup({ keys = "etovxqpdgfblzhckisuran;" })
 end
 
 return M
